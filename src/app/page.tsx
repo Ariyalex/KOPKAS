d8ce79e862055b76a5449aecb6e6466a620544ab
Ariyalex <ariya17duta@gmail.com>
2025-05-06 12:51:54 +0700

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">KOPKAS</h1>
-      <p className="mt-3 text-lg">fadsfdsaadbsdsnofp</p>
+      <p className="mt-3 text-lg">percobaan ubaghgasdjdsiaf</p>
     </main>
   );
 }
